93f19291d54dfedeaab6935f6ef4532820eaf86e
Jacob Robbins <jacob.d.robbins@gmail.com>
2013-02-14 09:42:17 -0500

    Ported plugin to Phonegap >= 2.0.0

---
+++ v2.0.0/clipboardmanager.js
@@ -0,0 +1,17 @@
+/**
+ * Phonegap ClipboardManager plugin
+ * Omer Saatcioglu 2011
+ * Guillaume Charhon - Smart Mobile Software 2011
+ * Jacob Robbins - Phonegap 2.0 port 2013
+ */
+
+
+window.clipboardManagerCopy = function(str, success, fail) {
+	cordova.exec(success, fail, "ClipboardManagerPlugin", "copy", [str]);
+};
+
+window.clipboardManagerPaste = function(success, fail) {
+	cordova.exec(success, fail, "ClipboardManagerPlugin", "copy", []);
+};
+
+
