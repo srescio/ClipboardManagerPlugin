e5bc869941f48151432330d9f54d6bd9b77072fa
Jacob Robbins <jacob.d.robbins@gmail.com>
2013-05-31 14:43:54 -0400

    switch Clipboard Manager class to derive from CordovaPlugin instead of deprecated Plugin class

--- v2.0.0/ClipboardManagerPlugin.java
+++ v2.0.0/ClipboardManagerPlugin.java
@@ -14,12 +14,12 @@ import android.content.Context;
 import android.text.ClipboardManager;
 
 
-import org.apache.cordova.api.Plugin;
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.CallbackContext;
 import org.apache.cordova.api.PluginResult;
 
 
-
-public class ClipboardManagerPlugin extends Plugin {
+public class ClipboardManagerPlugin extends CordovaPlugin {
 	private static final String actionCopy = "copy";
 	private static final String actionPaste = "paste";
 	private static final String errorParse = "Couldn't get the text to copy";
@@ -28,23 +28,16 @@ public class ClipboardManagerPlugin extends Plugin {
 	private ClipboardManager mClipboardManager;
 
 
-	/**
-	 * Executes the request and returns PluginResult.
-	 *
-	 * @param action
-	 *            The action to execute.
-	 * @param args
-	 *            JSONArry of arguments for the plugin.
-	 * @param callbackId
-	 *            The callback id used when calling back into JavaScript.
-	 * @return A PluginResult object with a status and message.
-	 */
-	public PluginResult execute(String action, JSONArray args, String callbackId) {
+	@Override
+	  public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
+	  {
+	
 		// If we do not have the clipboard
-		if(mClipboardManager == null)
-			// get it
+		if(mClipboardManager == null) {
 			mClipboardManager = (ClipboardManager) cordova.getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
-
+		}
+		
+	
 		// Copy
 		if (action.equals(actionCopy)) {
 			String arg = "";
@@ -52,23 +45,28 @@ public class ClipboardManagerPlugin extends Plugin {
 				arg = (String) args.get(0);
 				mClipboardManager.setText(arg);
 			} catch (JSONException e) {
-				return new PluginResult(PluginResult.Status.ERROR, errorParse);
+			      callbackContext.error( errorParse);
 			} catch (Exception e) {
-				return new PluginResult(PluginResult.Status.ERROR, errorUnknown);
+			      callbackContext.error( errorUnknown);
 			}
-			return new PluginResult(PluginResult.Status.OK, arg);
-
+			callbackContext.success();
 		// Paste
 		} else if (action.equals(actionPaste)) {
 			String arg = (String) mClipboardManager.getText();
 			if (arg == null) {
 				arg = "";
 			}
-			return new PluginResult(PluginResult.Status.OK, arg);
+			PluginResult copy_ret = new PluginResult(PluginResult.Status.OK, arg);
+			callbackContext.sendPluginResult(copy_ret);
+			callbackContext.success();
+			
 		} else {
-			return new PluginResult(PluginResult.Status.INVALID_ACTION);
+		      callbackContext.error("invalid action");
+		      return false;
 		}
-	}
+
+		return true;
+	 }
 }
 
 
